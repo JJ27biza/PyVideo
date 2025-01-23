609811443b8f7b7887f217a9474ced9076f524d2
JJ27biza <pereiracorbaldani@gmail.com>
2025-01-23 12:52:06 +0100

    serverFake_retoques

--- Grafic/Menu.py
+++ Grafic/Menu.py
@@ -1,13 +1,17 @@
 import os.path
+from operator import truediv
+
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 import sys
+
+from numpy.lib.utils import source
+
 sys.path.append('C:/Users/micro/PycharmProjects/PyVideo/')
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.slider import Slider
-from kivy.uix.video import Video
 from kivy.core.window import Window
 from Directory.DeleteArchiveDirectory import DeleteArchiveDirectory
 from Directory.OpenDirectory import OpenDirectory
@@ -19,9 +23,18 @@ from Directory.AddArchiveDirectory import AddArchiveDirectory
 from ActionVideo import ActionVideo
 from ffpyplayer.player import MediaPlayer
 from ControlSound.ActionSound import ActionSound
+import cv2
+import numpy as np
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
 
 class Menu(App):
     def build(self):
+        self.capture = None
+        self.pauseAction = False
+        self.buttonPlay = None  # Asumimos que tienes el botón Play/Pause
+        self.listVideo = []  # Lista de videos disponibles en el directorio
+        self.video_path = '../Image/pause.png'
         Window.maximize()
         Window.clearcolor = (0.2, 0.3, 0.4, 1)
         self.title = 'PyVideo'
@@ -39,14 +52,18 @@ class Menu(App):
         self.buttonAñadir.bind(on_press=self.on_press_Añadir)
         self.boxNav.add_widget(self.buttonAñadir)
         # Botón Añadir Bluethoot
-        self.buttonBluethoot=Button(text="Añadir Bluetooth",size_hint=(0.1,1))
-        self.boxNav.add_widget(self.buttonBluethoot)
+        self.buttonEnviar=Button(text="Enviar Video",size_hint=(0.1,1))
+        self.boxNav.add_widget(self.buttonEnviar)
+
+        self.buttonRecibir = Button(text="Recibir Video", size_hint=(0.1, 1))
+        self.boxNav.add_widget(self.buttonRecibir)
+
         # Botón Enviar Bluetooth
-        self.buttonEnviarVideo=Button(text="Enviar Blutooth",size_hint=(0.1,1))
-        self.boxNav.add_widget(self.buttonEnviarVideo)
+        self.buttonEmitirLocal=Button(text="Emitir Local",size_hint=(0.1,1))
+        self.boxNav.add_widget(self.buttonEmitirLocal)
         # Botón Descargar Video
-        self.buttonDescargar=Button(text="Descargar Video",size_hint=(0.1,1))
-        self.boxNav.add_widget(self.buttonDescargar)
+        self.buttonEmitirYouTube=Button(text="Emitir YouTube",size_hint=(0.1,1))
+        self.boxNav.add_widget(self.buttonEmitirYouTube)
         # Botón Borrar Video
         self.buttonBorrarVideo=Button(text="Borrar",size_hint=(0.1,1))
         self.buttonBorrarVideo.bind(on_press=self.on_press_Borrar)
@@ -59,14 +76,10 @@ class Menu(App):
 
         self.box.add_widget(self.buttonAtras)
 
+        # Crear el widget de imagen donde se mostrará el video
+        self.img = Image(source=self.video_path)
 
-
-        # Video
-
-        self.video = Video(source= '../Image/pause.png',state="pause")
-
-
-        self.box.add_widget(self.video)
+        self.box.add_widget(self.img)
         # Botón Delante
         self.buttonDelante = Button(text="Delante", size_hint=(0.1, 0.2))
         self.buttonDelante.pos_hint={'center_x': 0.5, 'center_y': 0.5}
@@ -81,12 +94,12 @@ class Menu(App):
         #Boton de suma de tiempo
         self.sumarVideo=Button(text="Más 30s", size_hint=(0.5, 1))
         self.sumarVideo.pos_hint={'center_x': 0.5, 'center_y': 0.5}
-        self.sumarVideo.bind(on_press=self.on_click_sum)
+        self.sumarVideo.bind(on_press=self.adelantar_30s)
         self.boxBack.add_widget(self.sumarVideo)
         #Botón para reducir tiempo
         self.restarVideo = Button(text="Menos 30s", size_hint=(0.5, 1))
         self.restarVideo.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
-        # self.restarVideo.bind(on_press=self.on_button_atras)
+        self.restarVideo.bind(on_press=self.atrasar_30s)
         self.boxBack.add_widget(self.restarVideo)
         # Botón Play (en BoxBack)
         self.buttonPlay = Button(text="Play", size_hint=(0.5, 1))
@@ -115,53 +128,58 @@ class Menu(App):
     def on_button_atras(self,instance):
         global numero
         try:
-            if 0 < numero:
-                 numero-=1
-                 self.video.source='../VideoStore/'+listVideo[numero]
-                 self.video.state = 'stop'
-                 self.video.state = 'play'
-                 print(f"Reproduciendo: {listVideo[numero]}",'Numero: ',numero)
-                 print('Atras', numero)
+            # Comprobar si el índice 'numero' es menor que el tamaño de la lista - 1
+            if numero < len(listVideo) + 1:
+                numero -= 1  # Avanzar al siguiente video
+                self.video_path = '../VideoStore/' + listVideo[numero]
+                self.capture = cv2.VideoCapture(self.video_path)  # Cargar el siguiente video
+                print(f"Reproduciendo: {listVideo[numero]}", 'Numero: ', numero)
             else:
                 print('No existen más videos')
         except Exception as error:
-            print('Error al siguiente video',error)
-    def on_button_adelante(self,instance):
+            print('Error al adelantar el video', error)
+
+
+    def on_button_adelante(self, instance):
         global numero
-
         try:
-            if (len(listVideo)-1)>numero:
-                numero += 1
-                self.video.source = '../VideoStore/'+listVideo[numero]
-                self.video.state = 'stop'
-                self.video.state = 'play'
-                print(f"Reproduciendo: {listVideo[numero]}",'Numero: ',numero)
-                print('Adelante',numero)
+            # Comprobar si el índice 'numero' es menor que el tamaño de la lista - 1
+            if numero < len(listVideo) -1:
+                numero += 1  # Avanzar al siguiente video
+                self.video_path= '../VideoStore/' + listVideo[numero]
+                self.capture = cv2.VideoCapture(self.video_path)  # Cargar el siguiente video
+                print(f"Reproduciendo: {listVideo[numero]}", 'Numero: ', numero)
             else:
-
                 print('No existen más videos')
-
         except Exception as error:
-            print('Error al siguiente video',error)
+            print('Error al adelantar el video', error)
 
     def on_button_press(self, instance):
-        if self.video.source == '../Image/pause.png' or not os.path.isdir('../VideoStore'):
+        # Comprobar si ya se ha cargado el video
+        if self.capture is None or not self.capture.isOpened():
             if len(listVideo) != 0:
-                self.video.source = '../VideoStore/' + listVideo[numero]
-                self.video.state = 'play'
-                self.buttonPlay.text = "Pause"
+                # Si no se ha cargado el video, cargar el video actual desde la lista
+                self.capture = cv2.VideoCapture('../VideoStore/' + listVideo[numero])
+                self.video_path='../VideoStore/' + listVideo[numero]
+                Clock.schedule_interval(self.update, 1.0 / 30.0)
             else:
-                print('Nose encuentra directorio')
-        elif self.video.state == 'play':
-            self.video.state = 'pause'
-            self.buttonPlay.text = "Play"
+                print('No se encuentra el directorio')
         else:
-            self.video.state = 'play'
-            self.buttonPlay.text = "Pause"
-    def on_press_Añadir(self,instance):
+            # Si ya está cargado, solo alternar el estado de pausa
+            self.pauseAction = not self.pauseAction
+            self.buttonPlay.text = 'Play' if self.pauseAction else 'Pause'
 
+    def on_press_Añadir(self,instance):
         global listVideo
         try:
+            # Llamado cuando el botón "Cerrar la ejecución" es presionado
+            print("La aplicación se cerrará.")
+            self.capture=cv2.VideoCapture(0)
+            # Liberar el recurso de captura de OpenCV
+            if self.capture is not None:
+                self.capture.release()  # Libera el recurso de la cámara
+
+            # Detener la aplicación de Kivy
             self.stop()
             op.run()
             url_video = delete.functionDeleteText(op.label.text)
@@ -171,9 +189,9 @@ class Menu(App):
             print('Error al añadir', error)
     def on_press_Borrar(self,instance):
         try:
-            video_path = self.video.source
-
-            deleteVideo.functionDeleteArchive(video_path)
+            if self.video_path!='../Image/pause.png':
+                self.capture.release()
+                deleteVideo.functionDeleteArchive(self.video_path)
         except Exception as error:
             print('Error al borrar',error)
 
@@ -196,56 +214,136 @@ class Menu(App):
             self.box.remove_widget(self.buttonDelante)
 
             self.box.size_hint = (1, 1)
-            self.video.size_hint = (1, 1)
-            self.video.keep_ratio = False
-            self.video.allow_stretch = True
+            self.img.size_hint = (1, 1)
+            self.img.keep_ratio = False
+            self.img.allow_stretch = True
 
-            self.video.pos_hint = {'center_x': 0.5, 'center_y': 0.5}  # Centra el video en la pantalla
+            self.img.pos_hint = {'center_x': 0.5, 'center_y': 0.5}  # Centra el video en la pantalla
 
 
             # Maximiza la ventana
             Window.fullscreen=True
 
-            self.video.size = Window.size
+            self.img.size = Window.size
 
-            self.video.bind(on_touch_down=self.on_video_click)
+            self.img.bind(on_touch_down=self.on_video_click)
 
         except Exception as error:
             print('Error al maximizar el video:', error)
 
     def on_video_click(self,instance,touch):
         try:
-            self.box.remove_widget(self.video)
+            self.box.remove_widget(self.img)
             self.layout.remove_widget(self.box)
             self.boxNav.size_hint=(1, 0.1)
             self.layout.add_widget(self.boxNav)
             self.box.add_widget(self.buttonAtras)
-            self.box.add_widget(self.video)
+            self.box.add_widget(self.img)
             self.box.add_widget(self.buttonDelante)
             self.box.size_hint = (1, 0.8)
             self.layout.add_widget(self.box)
             self.layout.add_widget(self.boxBack)
             Window.fullscreen=False
-            self.video.unbind(on_touch_down=self.on_video_click)
+            self.img.unbind(on_touch_down=self.on_video_click)
 
         except Exception as error:
             print('Error al hacer click en el video',error)
 
-    def on_click_sum(self, instance):
-       try:
-           # Obtener la posición actual del video
-           current_position = self.video.position
 
-           # Adelantar 30 segundos (solo si no sobrepasa la duración del video)
-           new_position = current_position + 30
-           if new_position < self.video.duration:
-               self.video.position = new_position
-               self.video.seek(int(new_position),False)
-               print('Adelanto de ',new_position)
-           else:
-               self.video.position = self.video.duration  # Si supera la duración, pon el video al final
-       except Exception as error:
-           print('Error al adelantar el video',error)
+
+    def update(self, dt):
+        # Asegurarse de que el video esté cargado
+        if self.capture is None or not self.capture.isOpened():
+            return  # Si no se ha abierto el video, no hacer nada
+
+        if self.pauseAction:
+            # Si está pausado, no leer un nuevo frame y mostrar el último frame pausado
+            if self.paused_frame is not None:
+                # Solo mostrar el último frame pausado en el widget de Kivy (sin abrir una nueva ventana)
+
+                frame = self.paused_frame
+                frame = cv2.flip(frame, 0)
+
+                # Convertir el frame a formato adecuado para Kivy (de BGR a RGB)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                # Convertir el frame en una textura que pueda ser utilizada por Kivy
+                texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
+                texture.blit_buffer(frame.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
+
+                # Mostrar la textura en el widget de imagen
+                self.img.texture = texture
+            return  # No leer un nuevo frame mientras está pausado
+
+        # Si no está pausado, leer un nuevo frame
+        ret, frame = self.capture.read()
+
+        if ret:
+            # Guardar el frame actual antes de continuar (para poder pausarlo)
+            self.paused_frame = frame
+
+            # Voltear el frame horizontalmente (flip horizontal)
+            frame = cv2.flip(frame, 0)
+
+            # Convertir el frame a formato adecuado para Kivy (de BGR a RGB)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Convertir el frame en una textura que pueda ser utilizada por Kivy
+            texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='rgb')
+            texture.blit_buffer(frame.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
+
+            # Mostrar la textura en el widget de imagen
+            self.img.texture = texture
+        else:
+            # Si el video ha terminado (ret es False), mostrar la imagen
+            self.show_end_image()
+
+    def show_end_image(self):
+        # Cargar la imagen que quieres mostrar al final del video
+        end_image_path = '../Image/pause.png'  # Cambia la ruta a la imagen que deseas mostrar
+        end_image = cv2.imread(end_image_path)
+
+        # Convertir la imagen a formato adecuado para Kivy (de BGR a RGB)
+        end_image = cv2.cvtColor(end_image, cv2.COLOR_BGR2RGB)
+
+        # Crear la textura para la imagen final
+        texture = Texture.create(size=(end_image.shape[1], end_image.shape[0]), colorfmt='rgb')
+        texture.blit_buffer(end_image.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
+
+        # Mostrar la textura en el widget de imagen
+        self.img.texture = texture
+        self.capture.release()
+
+    def adelantar_30s(self,instance):
+        # Asegúrate de que el video está abierto
+        if self.capture is not None and self.capture.isOpened():
+            # Obtener la posición actual en milisegundos
+            current_time_ms = self.capture.get(cv2.CAP_PROP_POS_MSEC)
+
+            # Adelantar 30 segundos (30000 milisegundos)
+            new_time_ms = current_time_ms + 30000  # 30 segundos en milisegundos
+
+            # Establecer la nueva posición en milisegundos
+            self.capture.set(cv2.CAP_PROP_POS_MSEC, new_time_ms)
+
+            print(f"Adelantando 30 segundos: del tiempo {current_time_ms}ms al tiempo {new_time_ms}ms")
+    def atrasar_30s(self,instance):
+        # Asegúrate de que el video está abierto
+        if self.capture is not None and self.capture.isOpened():
+            # Obtener la posición actual en milisegundos
+            current_time_ms = self.capture.get(cv2.CAP_PROP_POS_MSEC)
+
+            # Adelantar 30 segundos (30000 milisegundos)
+            new_time_ms = current_time_ms - 30000  # 30 segundos en milisegundos
+
+            # Establecer la nueva posición en milisegundos
+            self.capture.set(cv2.CAP_PROP_POS_MSEC, new_time_ms)
+
+            print(f"Adelantando 30 segundos: del tiempo {current_time_ms}ms al tiempo {new_time_ms}ms")
+
+    def on_stop(self):
+        # Liberar la captura de video cuando se cierre la aplicación
+        self.capture.release()
 
 
 if __name__ == "__main__":
--- serverFake.py
+++ serverFake.py
@@ -2,26 +2,42 @@ import os
 import time
 import threading
 from flask import Flask, send_from_directory
-from future.standard_library import import_top_level_modules
+import socket
 
-##inicializar server al querer enviar un video y seleccionar un archivo de video que querrramos enviar
+#ip privada
+mi_ip=socket.gethostbyname(socket.gethostname())
 
-# Configura la ruta al video local
-video_path = '../VideoStore/Qué es una integral. Explicación desde cero - Matemáticas con Juan (720p, h264).mp4'  # Cambia esta ruta a la ruta de tu video
-video_filename = os.path.basename(video_path)
+video_directory = os.path.abspath(os.path.join(os.path.dirname(__file__),'VideoStore'))  # Ruta absoluta desde el directorio del script
+video_filename = '2024-12-25 20-30-52.mkv'  # Nombre del archivo de video
+
+# Verificar si el archivo existe
+if not os.path.exists(os.path.join(video_directory, video_filename)):
+    print(f"El archivo {video_filename} no se encuentra en la ruta especificada.")
+    exit(1)  # Termina el script si el archivo no existe
 
 # Crear una aplicación Flask para servir el video
 app = Flask(__name__)
 
+
 # Ruta para servir el archivo de video
 @app.route('/descarga')
 def serve_video():
-    mime_type = 'video/mp4' if video_filename.endswith('.mp4') else 'video/x-matroska'  # Cambia según tu tipo de archivo
+    # Verificar la ruta al archivo de video
+    print(f"Ruta al archivo de video: {video_directory}")
+
+    # Determinar el tipo MIME del archivo
+    mime_type = 'video/mp4' if video_filename.endswith('.mp4') else 'video/x-matroska'
     print(f"Sirviendo el archivo {video_filename} con MIME {mime_type}")
+
+    # Servir el archivo para descarga
+    return send_from_directory(video_directory, video_filename, as_attachment=True, mimetype=mime_type)
+
 
 # Función para iniciar el servidor HTTP
 def start_server():
-    app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)  # `use_reloader=False` evita la reinicialización en desarrollo
+    app.run(host='0.0.0.0', port=5000, threaded=True,
+            use_reloader=False)  # `use_reloader=False` evita la reinicialización en desarrollo
+
 
 # Iniciar el servidor HTTP en un hilo separado
 server_thread = threading.Thread(target=start_server)
@@ -31,10 +47,11 @@ server_thread.start()
 # Esperar a que el servidor se inicie
 time.sleep(2)
 
-
-video_url = 'http://192.168.8.103:5000/video'  # Usa la IP local de tu máquina
-#añadir url en patalla para copiar y pegar en el dispositivo destinatario
+# Puedes mostrar la URL para copiar y pegar en otro dispositivo
+video_url = 'http://'+mi_ip+':5000/descarga'
+print(f"Accede a la URL para descargar el video: {video_url}")
 
 # Evitar que el servidor Flask se cierre
 while True:
     time.sleep(60)
+
